af5f44e3ca76127851855f0ff5395bf0ac2a90cf
manoj-b-22 <manojsrinivas.b19@iiits.in>
2022-04-27 13:06:02 +0530

    docker final changes

--- backend_librarymangement/config/redis.js
+++ backend_librarymangement/config/redis.js
@@ -1,17 +1,17 @@
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({ socket: { port: 6379, host: 'redisdb' } });
 
 (async () => {
   await client.connect();
 })();
 
-client.on("connect", () => {
-  console.log("Connected to Redis Server");
-});
-
 client.on("error", (err) => {
   console.log(`Error:${err}`);
+});
+
+client.on("connect", () => {
+  console.log("Connected to Redis Server");
 });
 
 module.exports = client;
